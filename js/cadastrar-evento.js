3239f654662158f5912c8b24403d1ca36d3c3593
apoloduarte <apololeonard@gmail.com>
2023-06-10 14:05:13 -0300

    finese da funcao cadastrar evento

--- js/cadastrar-evento.js
+++ js/cadastrar-evento.js
@@ -1,42 +1,47 @@
-/*const botaoEnviar = document.querySelector('.btn-primary');
+
+const botaoEnviar = document.querySelector(".btn-primary");
 const botaoSelector = document.querySelector('#descricao');
 
-botaoEnviar.addEventListener("click", () => cadastrarEvento())
+botaoEnviar.addEventListener("click", () => cadastrarEvento());
+
 
 function cadastrandoEvento(url, corpo) {
     fetch(url, {
         method: 'POST',
         headers: { 'Content-type': "application/json", },
-        body: JSON.stringify(corpo)
+        body: JSON.stringify(corpo),
     })
         .then(() => console.log(JSON.stringify(corpo)))
         .then(() => alert("Evento Cadastrado com sucesso"))
         .then(() => window.location.href = "admin.html")
         .catch((error) => alert("Não foi possível realizar o cadastro, tente novamente!"))
+
 }
 
 function cadastrarEvento() {
     event.preventDefault()
-    const url = "https://soundgarden-api.vercel.app/events"
-    const nome = document.querySelector('#nome').value
-    const atracoes = document.querySelector('#atracoes').value.split(", ")
-    const descricao = document.querySelector('#descricao').value
-    const data = document.querySelector('#data').value
-    const lotacao = document.querySelector('#lotacao').value
+    const url = "https://soundgarden-api.vercel.app/events";
+    const nomeSelector = document.querySelector('#nome').value;
+    const atracoesSelector = document.querySelector('#atracoes').value.split(", ");
+    const descricaoSelector = document.querySelector('#descricao').value;
+    const dataSelector = document.querySelector('#data').value;
+    const lotacaoSelector = document.querySelector('#lotacao').value;
    
-    corpo = {
-        "name": nome, 
+    corpo =
+    {
+        "name": nomeSelector, 
         "poster": "https://i.imgur.com/fQHuZuv.png",
-        "attractions": atracoes,
-        "description": descricao,
-        "scheduled": data,
-        "numer_tickets": lotacao
+        "attractions": atracoesSelector,
+        "description": descricaoSelector,
+        "scheduled": dataSelector,
+        "number_tickets": lotacaoSelector
     }
 
     cadastrandoEvento(url, corpo)
     
 }
-*/
+
+/*
 const btnSubmit = document.querySelector(".btn-primary");
 const descriptionSelector = document.querySelector('#descricao');
 
@@ -54,8 +59,8 @@ function fazPost(url, corpo) {
         .then(() => window.location.href = "admin.html")
         .catch((error) => alert('Não foi possível realizar o cadastro, tente novamente'))
 
-}
-
+}*/
+/*
 function cadastrarEvento() {
     event.preventDefault()
     const url = "https://soundgarden-api.vercel.app/events";
@@ -75,4 +80,4 @@ function cadastrarEvento() {
         "number_tickets": capacitySelector
     }
     fazPost(url, corpo);
-}
+}*/
